9b1274bc10ea480f1ef68afddc99aef0f496eed5
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-05 20:28:59 +1100

    Add some commented out code

--- src/Trezor.Net.UnitTest/UnitTest - NET.cs
+++ src/Trezor.Net.UnitTest/UnitTest - NET.cs
@@ -19,9 +19,11 @@ namespace Trezor.Net
             while (trezorDeviceInformation == null)
             {
                 var devices = WindowsHidDevice.GetConnectedDeviceInformations();
-                //var trezors = devices.Where(d => d.VendorId == TrezorManager.TrezorVendorId && TrezorManager.TrezorProductId == d.ProductId).ToList();
-                //trezorDeviceInformation = trezors.FirstOrDefault(t => t.UsagePage == TrezorManager.AcceptedUsagePages[0]);
 
+                //1.6.x Device By Id
+                //trezorDeviceInformation = devices.FirstOrDefault(d => d.DevicePath == @"\\?\hid#vid_534c&pid_0001&mi_00#7&b861b22&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}");
+
+                //1.7.x Device By Vid
                 var enumerable = devices.Where(d => d.DevicePath.Contains("1209")).ToList();
                 trezorDeviceInformation = enumerable.FirstOrDefault();
 
