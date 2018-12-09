b06090276a546c78b852e8940cab2cbbe3b7ff78
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-10 07:42:50 +1100

    Libusb

---
+++ src/Trezor.Net.UnitTest/LibUsbDevice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hid.Net
+{
+    public class LibUsbDevice : IDevice
+    {
+        public event EventHandler Connected;
+        public event EventHandler Disconnected;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetIsConnectedAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> ReadAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WriteAsync(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
